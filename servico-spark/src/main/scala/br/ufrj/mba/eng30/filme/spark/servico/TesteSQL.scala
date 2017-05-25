32dc6f08143a7353e7790018c049c53c193ae8dd
Henrique <henrique.s.andrade1@gmail.com>
2017-05-25 01:17:39 -0300

    teste com persistencia e CSV atraves do Thriftserver

--- servico-spark/src/main/scala/br/ufrj/mba/eng30/filme/spark/servico/TesteSQL.scala
+++ servico-spark/src/main/scala/br/ufrj/mba/eng30/filme/spark/servico/TesteSQL.scala
@@ -1,19 +1,51 @@
 package br.ufrj.mba.eng30.filme.spark.servico
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types._
 
 object TesteSQL {
 
    def main(args: Array[String]) {
 
+    //https://spark.apache.org/docs/latest/sql-programming-guide.html#hive-tables
+    
     val spark = SparkSession
-      .builder
-      .appName("TesteScalaSpark")
+      .builder()
+      .appName("TesteHive")
+      //.config("spark.sql.warehouse.dir", "hdfs://hadoop-master:9000/mba/dados/hive-warehouse")
+      //.config("spark.sql.warehouse.dir", "/opt/mba/spark/tmp-trabalho/tmp-shell/spark-warehouse/")
+      .enableHiveSupport()
       .getOrCreate()
 
     val sc = spark.sparkContext
 
     import spark.implicits._
+    import spark.sql
+    
+    //deletar a tabela para fins de teste
+    spark.sql("DROP TABLE IF EXISTS cliente")
+    
+    //criar o schema
+    val structCli = StructType(
+                  StructField("idcli",StringType) :: 
+                  StructField("cidade",StringType) ::
+                  StructField("sexo",StringType) ::
+                  StructField("idade",StringType) ::Nil)
+                  
+    //criar um DataFrame a partir de um CSV
+    val cliDf = spark.read.schema(structCli).csv("hdfs://hadoop-master:9000/mba/teste/clientes.csv")
+    
+    cliDf.show
+    
+    //criar tabela temporaria
+    cliDf.createOrReplaceTempView("clienteTemp")
+    
+    //criar a tabela "permanente" para que seja possível consultá-la via JDBC
+    spark.sql("create table cliente as select * from clienteTemp")
+    
+    //exibir resutlado no console
+    spark.sql("select * from cliente").show
 
     spark.close();
   }
