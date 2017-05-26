c675246d5bf5b3856e8a56ee45eaee843b94c8c5
Henrique <henrique.s.andrade1@gmail.com>
2017-05-26 01:46:46 -0300

    Teste Observer com SQL

---
+++ servico-spark/src/main/scala/br/ufrj/mba/eng30/filme/spark/servico/TesteObserverSQL.scala
@@ -0,0 +1,54 @@
+package br.ufrj.mba.eng30.filme.spark.servico
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import spark.jobserver.api.{ SparkJob => NewSparkJob, _ }
+import com.typesafe.config.Config
+import org.scalactic._
+import scala.util.Try
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+object TesteObserverSQL extends NewSparkJob {
+
+  type JobData = Seq[String]
+  type JobOutput = String
+
+  def runJob(sc: SparkContext, runtime: JobEnvironment, data: JobData): JobOutput = {
+    val spark = SparkSession
+      .builder()
+      .appName("TesteObserverSQL")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    //sc.parallelize(data).countByValue
+      
+    //criar o schema
+    val structCli = StructType(
+                  StructField("idcli",StringType) :: 
+                  StructField("cidade",StringType) ::
+                  StructField("sexo",StringType) ::
+                  StructField("idade",StringType) ::Nil)      
+
+    //criar um DataFrame a partir de um CSV
+    val cliDf = spark.read.schema(structCli).csv("hdfs://hadoop-master:9000/mba/teste/clientes.csv")
+    
+    cliDf.show
+    
+    //criar tabela temporaria
+    cliDf.createOrReplaceTempView("clienteTemp")
+
+    spark.sql("select * from clienteTemp").show
+    
+    spark.close()
+                  
+    "Resultado nomal"
+  }
+
+  def validate(sc: SparkContext, runtime: JobEnvironment, config: Config): JobData Or Every[ValidationProblem] = {
+    Try(config.getString("input.string").split(" ").toSeq)
+      .map(words => Good(words))
+      .getOrElse(Bad(One(SingleProblem("No input.string param"))))
+  }
+
+}
